4a8d7f5296b1782e4c4f2b75b75cf08cd899af76
Pugach2017 <santangels2@gmail.com>
2018-02-14 00:31:34 +0200

    indx

---
+++ HT2/js/script.js
@@ -0,0 +1,25 @@
+﻿const resort = prompt('Please select resort from 1 to 3',);{
+	if (resort === '1')
+		{alert('Dear customer, you select "Taba" resort!');
+	const choose = prompt('Will you confirm it? (Y/N)',);{
+		if (choose === 'Y') {alert('Enjoy your trip!');}
+		else if (choose === 'N') {alert('Please, select another resort');}
+		}
+		}
+		else if (resort === '2')
+		{alert('Dear customer, you select "Hurgada" resort!');
+	const choose = prompt('Will you confirm it? (Y/N)',);{
+		if (choose === 'Y') {alert('Enjoy your trip!');}
+		else if (choose === 'N') {alert('Please, select another resort');}
+		}
+		}
+		else if (resort === '3')
+		{confirm('Dear customer, you select "Sharm" resort!');{
+			сonfirm('Please, confirm you choise.');
+			}
+			}
+			else if (resort>3)
+			{alert('Dear customer, you computer will be BLOCKED if you selection will be wrong!!!');
+		alert('Please, select resort from 1 to 3, or call us +38 099 999 88 77');
+		}
+		}
